63fa59ce6d8aa8924908b7f77f3eaab87c3fa923
DARWIN PROYECTOS <darwin.aguirre1@catolica.edu.sv>
2025-02-27 18:14:25 -0600

    creacion de clase reservas

---
+++ P01_2022-AG-652_2022-FS-651/Models/Reservas.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace P01_2022_AG_652_2022_FS_651.Models
+{
+    public class Reservas
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public DateTime Fecha { get; set; }
+        [Required]
+        public int CantidadHoras { get; set; }
+        [ForeignKey("Usuario")]
+        public int UsuarioId { get; set; }
+        public Usuarios Usuario { get; set; }
+        [ForeignKey("EspacioParqueo")]
+        public int EspacioId { get; set; }
+    }
+}
+
